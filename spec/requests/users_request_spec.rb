1e30ed34339d9e8988afbafc79de91aef8871b83
abeidahmed <abeidahmed92@gmail.com>
2021-01-03 18:41:46 +0530

    Make user create api

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Users", type: :request do
+  describe "#create" do
+    it "does create a new user if the fields are valid" do
+      post users_path, params: { user: attributes_for(:user).except(:auth_token) }
 
+      expect(User.count).to eq(1)
+      expect(cookies[:auth_token]).to eq(User.first.auth_token)
+    end
+
+    it "returns an error if fields are invalid" do
+      post users_path, params: { user: attributes_for(:user).except(:auth_token, :email_address) }
+
+      expect(json.dig(:errors, :email_address)).to be_present
+    end
+  end
 end
