46f8242fec324071991fb667addf4e026f038b80
abeidahmed <abeidahmed92@gmail.com>
2021-01-03 18:26:42 +0530

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+  before_validation :trim_white_spaces
+  before_save :normalize_email_address
+  before_create :generate_auth_token
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates(
+    :email_address,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+  )
+  validates :full_name, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+  private
+
+  def trim_white_spaces
+    self.email_address = email_address.to_s.strip
+    self.full_name     = full_name.to_s.strip
+  end
+
+  def normalize_email_address
+    self.email_address = email_address.downcase
+  end
+
+  def generate_auth_token
+    generate_token(:auth_token)
+  end
+
+  def generate_token(column)
+    loop do
+      self[column] = SecureRandom.urlsafe_base64
+      break unless User.exists?(column => self[column])
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe "validations" do
+    it { is_expected.to have_secure_password }
+
+    it { is_expected.to validate_presence_of(:email_address) }
+
+    it { is_expected.to validate_uniqueness_of(:email_address).case_insensitive }
+
+    it { is_expected.to validate_length_of(:email_address).is_at_most(255) }
+
+    it { is_expected.to allow_value("abeidmama@example.com", "abeidm@em.uk.com").for(:email_address) }
+
+    it { is_expected.not_to allow_value("abeidmama", "abeidm@em@.com").for(:email_address) }
+
+    it { is_expected.to validate_length_of(:full_name).is_at_most(255) }
+
+    it "does lowercase email_address before saving" do
+      user.email_address = user.email_address.upcase
+      user.save!
+      expect(user.reload.email_address).to eq(user.email_address.downcase)
+    end
+
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+    context "when creating a new user" do
+      it "will set auth_token with Secure Random hash" do
+        user.auth_token = nil
+        user.save!
+        expect(user.reload.auth_token).not_to be_nil
+      end
+
+      it "will trim excess white spaces from email address" do
+        user.email_address = "       email@example.com "
+        user.save!
+        expect(user.email_address).to eq("email@example.com")
+      end
+
+      it "will trim excess white spaces from full name" do
+        user.full_name = "   John Doe "
+        user.save!
+        expect(user.full_name).to eq("John Doe")
+      end
+    end
+  end
 end
