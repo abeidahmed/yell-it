d1429643ca02ee38d760cf49196ae95d72751ac9
abeidahmed <abeidahmed92@gmail.com>
2021-01-03 19:02:16 +0530

    Rename method due to conflict

--- spec/support/features/sessions_helper.rb
+++ spec/support/features/sessions_helper.rb
@@ -1,6 +1,6 @@
 module Features
   module SessionsHelper
-    def sign_in(user = nil)
+    def feature_sign_in(user = nil)
       user ||= create(:user)
       visit new_session_path
       fill_in "email_address", with: user.email_address
