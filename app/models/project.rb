6868d9c8a61a4a61282a4529ccd0dff94dbbcf72
abeidahmed <abeidahmed92@gmail.com>
2021-01-04 16:50:05 +0530

    Add project model validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ApplicationRecord
+  before_save :normalize_subdomain
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :subdomain, length: { maximum: 63 }, uniqueness: { case_sensitive: false }
+
+  private
+
+  def normalize_subdomain
+    self.subdomain = subdomain.to_s.downcase
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:project) { build(:project) }
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_length_of(:name).is_at_most(255) }
+
+    it { is_expected.to validate_length_of(:subdomain).is_at_most(63) }
+
+    it { is_expected.to validate_uniqueness_of(:subdomain).case_insensitive }
+  end
+
+  describe "before saving" do
+    it "does lowercase subdomain" do
+      subdomain = "helloworld"
+      project   = create(:project, subdomain: subdomain.upcase)
+      expect(project.reload.subdomain).to eq(subdomain)
+    end
+
+    it "does not throw error when lowercasing nil values" do
+      project = create(:project, subdomain: nil)
+      expect(project.reload.subdomain).to eq("")
+    end
+  end
 end
