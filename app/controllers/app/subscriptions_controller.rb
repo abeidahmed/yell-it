24580700c9fed2397b637cd2aa4e7858f7162c87
abeidahmed <abeidahmed92@gmail.com>
2021-01-05 22:22:20 +0530

    Handle card errors

--- app/controllers/app/subscriptions_controller.rb
+++ app/controllers/app/subscriptions_controller.rb
@@ -4,7 +4,7 @@ class App::SubscriptionsController < App::BaseController
   def new; end
 
   def create
-    project = Project.find("d3025110-1ffa-4c84-bc47-48c680289147")
+    project = Project.find("528825e9-5e5d-4517-bcfc-56d7fb325813")
 
     customer =
       if project.stripe_customer_id?
@@ -43,12 +43,20 @@ class App::SubscriptionsController < App::BaseController
     if subscription.status == "active"
       redirect_to app_confirm_subscription_path(payment_intent: intent_id)
     else
-      confirm_intent = Stripe::PaymentIntent.confirm(intent_id, {
-        payment_method: params[:pm_token],
-        return_url: app_confirm_subscription_url
-      })
+      begin
+        confirm_intent = Stripe::PaymentIntent.confirm(intent_id, {
+          payment_method: params[:pm_token],
+          return_url: app_confirm_subscription_url
+        })
 
-      redirect_to confirm_intent.next_action.redirect_to_url.url
+        if confirm_intent.next_action
+          redirect_to confirm_intent.next_action.redirect_to_url.url
+        else
+          redirect_to app_confirm_subscription_path(payment_intent: intent_id)
+        end
+      rescue Stripe::CardError => e
+        redirect_back fallback_location: root_path, alert: { message: e.error.message }
+      end
     end
   end
 
