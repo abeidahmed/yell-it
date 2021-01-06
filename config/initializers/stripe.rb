cfeb3901179214f3bb8e875b63f5cfca371973d6
abeidahmed <abeidahmed92@gmail.com>
2021-01-06 10:35:54 +0530

    Move PaymentSucceeded to stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,14 @@
 Stripe.api_key             = Rails.application.credentials.stripe.dig(:dev, :secret_key)
 StripeEvent.signing_secret = Rails.application.credentials.stripe.dig(:dev, :signing_secret)
 
+class PaymentSucceeded
+  def call(event)
+    object  = event.data.object
+    project = Project.find(stripe_subscription_id: object.subscription)
+    project.premium!
+  end
+end
+
 StripeEvent.configure do |events|
   events.subscribe "invoice.payment_succeeded", PaymentSucceeded.new
 end
