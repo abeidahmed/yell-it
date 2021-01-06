964c9e0b5ff232a906cc757ebc518219f95e0bc2
abeidahmed <abeidahmed92@gmail.com>
2021-01-06 10:43:16 +0530

    Check for pundit scope usage

--- app/controllers/app/base_controller.rb
+++ app/controllers/app/base_controller.rb
@@ -1,3 +1,14 @@
 class App::BaseController < ApplicationController
   layout "app"
+
+  before_action :authenticate_user
+
+  after_action :verify_authorized, except: %i[index] # rubocop:disable Rails/LexicallyScopedActionFilter
+  after_action :verify_policy_scoped, only: %i[index] # rubocop:disable Rails/LexicallyScopedActionFilter
+
+  private
+
+  def authenticate_user
+    redirect_to new_session_path, alert: { message: "Please signup or login to continue" } unless user_signed_in?
+  end
 end
